6822e5e2c1918d776118554c43f1992b4f7e91cf
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-06-23 18:46:51 -0600

    Added a benchmark for using protbuf.

--- README.md
+++ README.md
@@ -9,8 +9,14 @@ Currently the following encodings are being compared:
 
 * JSON - Uses the built in JSON encoding library
 * BSON - Uses [gobson](http://labix.org/gobson)
+* Protobuf - Uses [goprotobuf](https://code.google.com/p/goprotobuf/)
 
 ### Planned
 
-* Protobuf
 * Capnproto
+
+### Running
+
+To run the code, make sure that protbuf is installed.  Once it's install run `./build.sh`.  This will create the go protobuf files and build an executable.  To run the benchmarks run``go_marshal_bench`
+
+To read the output, the `Internal` label means that it is using the non protobuf generated struct.  I wanted to see if there would be a performance different between serialzing the protobuf generated struct vs a non protobuf generated struct.
--- main.go
+++ main.go
@@ -4,39 +4,52 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
+//	"os"
 	"time"
 
+	"code.google.com/p/goprotobuf/proto"
 	"labix.org/v2/mgo/bson"
 )
 
-type TestStruct struct {
-	Name      string
-	CreatedOn time.Time
-	Number    uint64
+type InternalMessage struct {
+	Name             string `json:"name,omitempty"`
+	CreatedOn        int64  `json:"createdOn,omitempty"`
+	Number           uint64 `json:"number,omitempty"`
 }
 
 type BenchSuite struct {
 	N    int
-	data []TestStruct
+	data []Message
+	internalData []InternalMessage
 }
 
 func NewBenchSuite(size int) BenchSuite {
-	data := SetUpSuite(size)
-	return BenchSuite{size, data}
+	data := SetUpMessages(size)
+	internalData := SetUpInternalMessages(size)
+	return BenchSuite{size, data, internalData}
 }
 
-func SetUpSuite(size int) []TestStruct {
+func SetUpMessages(size int) []Message {
 	fmt.Printf("Setting up test data with %d entries\n", size)
-	data := make([]TestStruct, size, size)
+	data := make([]Message, size, size)
 	for i := 0; i < size; i++ {
-		data[i] = TestStruct{"A Name", time.Now(), 1234567890}
+		data[i] = Message{Name: proto.String("A Name"), CreatedOn: proto.Int64(time.Now().Unix()), Number: proto.Uint64(1234567890)}
+	}
+	return data
+}
+
+func SetUpInternalMessages(size int) []InternalMessage {
+	fmt.Printf("Setting up test data with %d entries\n", size)
+	data := make([]InternalMessage, size, size)
+	for i := 0; i < size; i++ {
+		data[i] = InternalMessage{"A Name", time.Now().Unix(), 1234567890}
 	}
 	return data
 }
 
 func (s *BenchSuite) BenchmarkJson() {
 	for i := 0; i < s.N; i++ {
-		var result TestStruct
+		var result Message
 		encoding, err := json.Marshal(&s.data[i])
 		if err != nil {
 			log.Fatal(err)
@@ -50,8 +63,50 @@ func (s *BenchSuite) BenchmarkJson() {
 
 func (s *BenchSuite) BenchmarkBson() {
 	for i := 0; i < s.N; i++ {
-		var result TestStruct
+		var result Message
 		encoding, err := bson.Marshal(&s.data[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = bson.Unmarshal(encoding, &result)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func (s *BenchSuite) BenchmarkProtoBuf() {
+	for i := 0; i < s.N; i++ {
+		var result Message
+		encoding, err := proto.Marshal(&s.data[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = proto.Unmarshal(encoding, &result)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func (s *BenchSuite) BenchmarkInternalJson() {
+	for i := 0; i < s.N; i++ {
+		var result Message
+		encoding, err := json.Marshal(&s.internalData[i])
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = json.Unmarshal(encoding, &result)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func (s *BenchSuite) BenchmarkInternalBson() {
+	for i := 0; i < s.N; i++ {
+		var result Message
+		encoding, err := bson.Marshal(&s.internalData[i])
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -67,16 +122,36 @@ func runBench() {
 	for _, size := range sizes {
 		bench := NewBenchSuite(size)
 		startTime := time.Now()
+		bench.BenchmarkInternalJson()
+		fmt.Printf("Internal Json:\t%v\n", time.Since(startTime))
+		startTime = time.Now()
 		bench.BenchmarkJson()
-		fmt.Printf("Json: %v\n", time.Since(startTime))
+		fmt.Printf("Json:\t\t%v\n", time.Since(startTime))
+		startTime = time.Now()
+		bench.BenchmarkInternalBson()
+		fmt.Printf("Internal Bson:\t%v\n", time.Since(startTime))
 		startTime = time.Now()
 		bench.BenchmarkBson()
-		fmt.Printf("Bson: %v\n", time.Since(startTime))
+		fmt.Printf("Bson:\t\t%v\n", time.Since(startTime))
+		startTime = time.Now()
+		bench.BenchmarkProtoBuf()
+		fmt.Printf("ProtoBuf:\t%v\n", time.Since(startTime))
 	}
 }
 
 func main() {
 	fmt.Printf("Starting Benchmark\n")
+
+/*
+	outWriter := json.NewEncoder(os.Stdout)
+	message := Message{Name: proto.String("A Name"), CreatedOn: proto.Int64(time.Now().Unix()), Number: proto.Uint64(1234567890)}
+	outWriter.Encode(message)
+	encoding, err := bson.Marshal(message)
+	if err != nil {
+		log.Fatal(err)
+	}
+	os.Stdout.Write(encoding)
+*/
 	runBench()
 	fmt.Printf("Finished Benchmark\n")
 }
